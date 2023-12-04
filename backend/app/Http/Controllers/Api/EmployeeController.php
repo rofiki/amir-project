70a9f32ac7438cfb89780da770d1ab6b7c4a757d
Rofiki Harong <vidocq.hrg@gmail.com>
2023-12-04 11:17:10 +0700

    crud employee เสร็จ

--- backend/app/Http/Controllers/Api/EmployeeController.php
+++ backend/app/Http/Controllers/Api/EmployeeController.php
@@ -26,4 +26,94 @@ class EmployeeController extends Controller
             return $this->returnError($e->getMessage());
         }
     }
+
+    public function show(Request $request, Employee $employee) //show by id
+    {
+        return new EmployeeResource($employee);
+    }
+
+    public function store(Request $request) // add
+    {
+        $validated = Validator::make($request->all(), [
+            // 'department_id' => 'required',
+            'employee_code' => 'required|min:2|max:255',
+            'prefix_id' => 'required',
+            'firstname' => 'required|min:2|max:255',
+            'lastname' => 'required|min:2|max:255',
+            // 'nickname' => 'required|min:2|max:255',
+            'gender' => 'required',
+            'date_of_birth' => 'required|min:2|max:255',
+            'idcard' => 'required|min:2|max:255',
+            'date_of_sign' => 'required|min:2|max:255',
+            // 'date_of_resign' => 'required|min:2|max:255',
+           
+        ]);
+        
+        if ($validated->fails()) {
+            $response = response()->json(['status' => false, 'error' => $validated->messages()], 422);
+        } else {
+            $employee = Employee::create([
+                'users_login_id' => $request->users_login_id,
+                'employee_code' => $request->employee_code,
+                'prefix_id' => $request->prefix_id,
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+                'nickname' => $request->nickname,
+                'gender' => $request->gender,
+                'date_of_birth' => $request->date_of_birth,
+                'idcard' => $request->idcard,
+                'date_of_sign' => $request->date_of_sign,
+                'date_of_resign' => $request->date_of_resign,
+                'active' => $request->active,
+            ]);
+            $response =new EmployeeResource($employee);
+            
+        }
+        return $response;
+
+        // users_login_id
+        // employee_code
+        // prefix_id
+        // firstname
+        // lastname
+        // nickname
+        // gender
+        // date_of_birth
+        // idcard
+        // date_of_sign
+        // date_of_resign
+        // active
+    }
+
+    public function update(Request $request, Employee $employee) // update
+    {
+        $validated = Validator::make($request->all(), [
+            'name' => 'required|min:2|max:255'
+        ]);
+        
+        if ($validated->fails()) {
+            $response = response()->json(['status' => false, 'error' => $validated->messages()], 422);
+        } else {
+
+            $employee->update([
+                'department_id' => $request->department_id,
+                'name' => $request->name,
+                'description' => $request->description
+            ]);
+
+            $response = new EmployeeResource($employee);
+        }
+        return $response;
+    }
+
+    public function destroy(Request $request, Employee $employee) // delete
+    {
+        try {
+            $employee->delete();
+            return response()->json(['status' => true,], 200);
+        }catch(\Exception $e){
+            return response()->json(['status' => false,], 404);
+        }
+        return response()->json(['status' => false,], 500);
+    }
 }
