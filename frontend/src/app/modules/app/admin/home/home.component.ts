8ce8c629b211604336164760a842eba898fb759c
rofiki <vidocq.hrg@gmail.com>
2023-12-08 01:10:39 +0700

    design เสร็จ

--- frontend/src/app/modules/app/admin/home/home.component.ts
+++ frontend/src/app/modules/app/admin/home/home.component.ts
@@ -1,4 +1,5 @@
-import {  Component, } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { AppService } from 'src/app/services/app.service';
 
 @Component({
@@ -6,10 +7,34 @@ import { AppService } from 'src/app/services/app.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public xtoggle:boolean = true;
 
   public BASE_URL: string = this.appService.BASE_URL;
 
-  constructor(private appService: AppService,){}
+  constructor(private appService: AppService, @Inject(DOCUMENT) private document: Document, private renderer: Renderer2,){}
+
+  ngOnInit(): void {
+    
+}
+
+// ngOnDestroy(): void {
+//     this.renderer.removeClass(this.document.body, 'sb-sidenav-toggled');
+// }
+
+toggle(){
+
+  this.xtoggle = this.xtoggle ? false : true;
+
+  if(this.xtoggle){
+    this.renderer.removeClass(this.document.body, 'sb-sidenav-toggled');
+  }
+
+  if(!this.xtoggle){
+    this.renderer.addClass(this.document.body, 'sb-sidenav-toggled');
+  }
+  
+}
 
 }
---
+++ frontend/src/assets/js/scripts.js
@@ -0,0 +1,45 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+// 
+
+// window.addEventListener('DOMContentLoaded', event => {
+    
+//     document.body.classList.toggle('sb-sidenav-toggled');
+
+//     document.body.classList.remove('sb-sidenav-toggled');
+    
+//     // console.log('aa',document.querySelector('button#sidebarToggle').toggle(''));
+
+    
+    
+//     // Toggle the side navigation
+//     const sidebarToggle = document.querySelector('button#sidebarToggle');
+
+//     const a = document.body;
+
+//     console.log('a',a)
+//     console.log('sidebarToggle2',sidebarToggle);
+
+//     if (sidebarToggle) {
+
+//         // Uncomment Below to persist sidebar toggle between refreshes
+//         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+//         //     document.body.classList.toggle('sb-sidenav-toggled');
+//         // }
+//         sidebarToggle.addEventListener('click', event => {
+            
+//             event.preventDefault();
+//             document.body.classList.toggle('sb-sidenav-toggled');
+//             // document.
+//             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            
+//         });
+//     }
+
+// });
+// this.renderer.addClass(this.document.body, 'embedded-body');
